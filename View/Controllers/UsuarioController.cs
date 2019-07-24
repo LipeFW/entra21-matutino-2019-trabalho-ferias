7f36d3d2d73f866bb64e1c6f6e5fcab8053e1627
Felipe Franco Weber <felipeweber2002@hotmail.com>
2019-07-24 01:10:54 -0300

    Criada classe Usuario e o UsuarioController

---
+++ View/Controllers/UsuarioController.cs
@@ -0,0 +1,20 @@
+﻿using Model;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Usuario> usuarios = repository.ObterTodos();
+            ViewBag.Usuarios = usuarios;
+            return View();
+        }
+    }
+}
